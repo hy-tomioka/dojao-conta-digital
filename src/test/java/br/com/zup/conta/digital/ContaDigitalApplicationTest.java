3afd86577404623b5cd7df1ebef5d2883fcdcd84
zlbovolini <lucas.bovolini@zup.com.br>
2021-10-06 19:29:32 -0300

    Adiciona cobertura de linhas de codigo

---
+++ src/test/java/br/com/zup/conta/digital/ContaDigitalApplicationTest.java
@@ -0,0 +1,11 @@
+package br.com.zup.conta.digital;
+
+import org.junit.jupiter.api.Test;
+
+class ContaDigitalApplicationTest {
+
+    @Test
+    void notATest() {
+        ContaDigitalApplication.main(new String[] {});
+    }
+}
