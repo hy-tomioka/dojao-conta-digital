858983c7dfdaec0c5632b70ddd27567a8920b0e2
zlbovolini <lucas.bovolini@zup.com.br>
2021-10-07 15:40:14 -0300

    Cria e popula base de dados

---
+++ docker/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO conta(id,numero, id_cliente,saldo) VALUES (1,'5555-1', 'UUIDBRABO', 150.0);
+INSERT INTO conta(id,numero, id_cliente,saldo) VALUES (2,'5556-1', 'UUIDBRABO', 250.0);
+INSERT INTO conta(id,numero, id_cliente,saldo) VALUES (3,'5557-1', 'UUIDBRABO', 350.0);
---
+++ docker/init.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS contas;
+
+create table conta (
+	id bigint not null auto_increment,
+	id_cliente varchar(255) not null,
+	numero varchar(255) not null unique,
+	saldo decimal(19,2) not null,
+	primary key (id)
+);
+
+create table transacao (
+	id bigint not null,
+	criado_em datetime(6) not null,
+	tipo_transacao varchar(255) not null,
+	uuid binary(255) not null unique,
+	valor decimal(19,2) not null,
+	conta_id bigint not null,
+	primary key (id),
+	foreign key (conta_id) references conta (id)
+);
